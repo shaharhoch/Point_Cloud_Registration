4009829dc765344726e9f5c43e3e4310204ce3d0
shaharhoch <shaharhoch@gmail.com>
2017-04-15 02:04:17 +0300

    Implemented gicp algorithm using PCL library.

--- VS_GICP/gicp_main.cpp
+++ VS_GICP/gicp_main.cpp
@@ -1,21 +1,70 @@
 #include <iostream>
+#include <fstream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/gicp.h>
 #include "mex.h"
 
+static const bool DEBUG = true; 
+
 //static function declearion:
 static void verifyInput(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]);
 static void getPointCloud(const double* in_mtx, int num_of_points, pcl::PointCloud<pcl::PointXYZ>::Ptr p_point_cloud);
+static void savePointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr p_point_cloud, const char* p_file_name);
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	mexPrintf("Bla\n");
-	/*pcl::PointCloud<pcl::PointXYZ>::Ptr global_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	verifyInput(nlhs, plhs, nrhs, prhs);
+
+	double epsilon = mxGetScalar(prhs[2]);
+	double d_max = mxGetScalar(prhs[3]);
+	
+	pcl::PointCloud<pcl::PointXYZ>::Ptr global_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr local_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
 	getPointCloud(mxGetPr(prhs[0]), mxGetM(prhs[0]), local_cloud);
-	getPointCloud(mxGetPr(prhs[1]), mxGetM(prhs[1]), global_cloud);*/
+	getPointCloud(mxGetPr(prhs[1]), mxGetM(prhs[1]), global_cloud);
+
+	if (DEBUG == true)
+	{
+		savePointCloud(local_cloud, "Local_Cloud.txt");
+		savePointCloud(global_cloud, "Global_Cloud.txt");
+	}
+	
+	pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> gicp;
+	gicp.setInputCloud(local_cloud);
+	gicp.setInputTarget(global_cloud);
+	gicp.setMaximumOptimizerIterations(100);
+	gicp.setMaxCorrespondenceDistance(d_max);
+
+	pcl::PointCloud<pcl::PointXYZ> Final;
+	pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ>::Matrix4 transform_matrix; 
+	gicp.align(Final, transform_matrix);
+
+	if (DEBUG == true)
+	{
+		mexPrintf("GICP algorithm converged? %s\n", gicp.hasConverged() ? "true" : "false");
+		mexPrintf("GICP algorithm fitness score: %lf\n", gicp.getFitnessScore());
+	}
+
+	// Prepare output arguments
+	//Currently I set number of iterations to always be 0, 
+	//because getting the number of iterations is not supported in PCL registration
+	plhs[1] = mxCreateDoubleScalar(0);
+	double* iter_out = mxGetPr(plhs[1]);
+	*iter_out = 0;
+
+	plhs[0] = mxCreateDoubleMatrix(4, 4, mxREAL);
+	double* p_out_mtx4 = mxGetPr(plhs[0]); 
+
+	//Copy transform matrix
+	for (int i = 0; i<4; i++)
+	{
+		for (int k = 0; k < 4; k++)
+		{
+			p_out_mtx4[i * 4 + k] = transform_matrix(k, i); 
+		}
+	}
 }
 
 // This function verifies the correctness of the inputs from matlab
@@ -81,6 +130,20 @@ static void getPointCloud(const double* in_mtx, int num_of_points, pcl::PointClo
 
 		p_point_cloud->push_back(pt); 
 	}
+}
+
+static void savePointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr p_point_cloud, const char* p_file_name)
+{
+	std::ofstream out_file; 
+	out_file.open(p_file_name);
+
+	for (size_t i = 0; i < p_point_cloud->size(); i++)
+	{
+		out_file << "(" << p_point_cloud->points[i].x << "," <<
+			p_point_cloud->points[i].y << "," << p_point_cloud->points[i].y
+			<< ")"; 
+	}
+	out_file.close();
 }
 
 int old(int argc, char** argv)
--- gicpWrapper.m
+++ gicpWrapper.m
@@ -3,6 +3,8 @@ function [ transform, max_iter ] = gicpWrapper( local_cloud,  global_cloud, ...
 
 [transform, max_iter] = gicp_alg(double(local_cloud.Location),...
     double(global_cloud.Location), gicp_epsilon, max_inliner_distance);
+    
+clear mex;
 
 transform = transpose(transform);
 end
